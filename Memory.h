2fd8bfa6d9b9d64cd3426950b0b1c48b155eda3c
Zach Champion <zachchampion79@gmail.com>
2021-12-03 16:08:35 -0500

    Begin building memory class

---
+++ Memory.h
@@ -0,0 +1,36 @@
+#ifndef MEMORY_H_
+#define MEMORY_H_
+
+#include "Contact.h"
+#include "Message.h"
+
+class Memory {
+ public:
+  Memory();
+  Memory(Contact node);
+  unsigned char* getNodeUUID();
+  char* getNodeName();
+  unsigned short getNumberContacts();
+  unsigned short getNumberMessages();
+  Contact getContact(unsigned short index);
+  Message getMessage(unsigned short index);
+  bool saveContact(Contact contact);
+  void saveMessage(Message message);
+  void saveNodeInformation(Contact contact);
+  // Add as you see fit
+
+ protected:
+  bool hasSchema();
+  void setSchema();
+  void clearMessages();
+  void clearContacts();
+  unsigned short getMessagePointerOffset();
+  // Add as you see fit
+
+ private:
+  const unsigned short MAX_CONTACTS = 10;
+  const unsigned short MAX_MESSAGES = 20;
+  // Add as you see fit
+};
+
+#endif
