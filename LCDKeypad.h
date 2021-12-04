a501817b9a43e289b28b667294e8b8dc1d5e50cf
Zach Champion <zachchampion79@gmail.com>
2021-12-04 15:00:51 -0500

    Add constructor

--- LCDKeypad.h
+++ LCDKeypad.h
@@ -5,6 +5,10 @@
 
 class LCDKeypad : public LiquidCrystal {
  public:
+  LCDKeypad(uint8_t rs, uint8_t enable,
+            uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+            uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, uint8_t btn);
+
   // identifies a button by its approximate analog value
   enum class Button : unsigned short { RIGHT = 0,
                                        UP = 145,
@@ -15,6 +19,10 @@ class LCDKeypad : public LiquidCrystal {
 
   // get which button is currently pressed
   Button getButtonPress();
+
+ private:
+  unsigned long long lastButtonUpdate;
+  uint8_t buttonPin;
 };
 
 #endif
