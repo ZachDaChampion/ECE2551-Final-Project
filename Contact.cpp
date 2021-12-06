4e8a6768abc7377d4fa77364ace6e1751a5a7cf3
Zach Champion <zachchampion79@gmail.com>
2021-12-05 23:24:44 -0500

    Fix dynamic memory issues

--- Contact.cpp
+++ Contact.cpp
@@ -1,47 +1,27 @@
 #include "Contact.h"
 
-Contact::Contact() {
-  uuid = new unsigned char[5];
-  name = new char[11];
+Contact::Contact() {}
+
+Contact::Contact(unsigned char* givenUUID, char const* givenName) {
+  setUUID(givenUUID);
+  setName(givenName);
 }
 
-Contact::Contact(const Contact& other)
-    : uuid(new unsigned char[5]), name(new char[10]) {
-  {
-    memcpy(uuid, other.uuid, 5);
-    memcpy(name, other.name, 10);
-  }
+Contact::Contact(unsigned char* givenUUID, char givenName) {
+  setUUID(givenUUID);
+  setName(givenName);
+}
 
-  Contact::Contact(unsigned char* givenUUID, char const* givenName)
-      : uuid(new unsigned char[5]), name(new char[11]) {
-    setUUID(givenUUID);
-    setName(givenName);
-  }
+void Contact::setUUID(unsigned char* givenUUID) {
+  memcpy(uuid, givenUUID, 5);
+}
 
-  Contact::Contact(unsigned char* givenUUID, char givenName)
-      : uuid(new unsigned char[5]),
-        name(new char[11]) {
-    setUUID(givenUUID);
-    setName(givenName);
-  }
-
-  Contact::~Contact() {
-    delete[] uuid;
-    delete[] name;
-  }
-
-  void Contact::setUUID(unsigned char* givenUUID) {
-    memcpy(uuid, givenUUID, 5);
-  }
-
-  void Contact::setName(char const* givenName) {
-    unsigned char i;
-    for (i = 0; i < 10; ++i) {
-      if (givenName[i] == '\0')
-        break;
-      name[i] = givenName[i];
-    }
-    name[i] = '\0';
+void Contact::setName(char const* givenName) {
+  unsigned char i;
+  for (i = 0; i < 11; ++i) {
+    if (givenName[i] == '\0')
+      break;
+    name[i] = givenName[i];
   }
 
   void Contact::setName(char givenName) {
--- Contact.h
+++ Contact.h
@@ -1,15 +1,11 @@
 #ifndef CONTACT_H_
 #define CONTACT_H_
 
-#include <iostream>
-
 class Contact {
  public:
   Contact();
-  Contact(const Contact& other);
   Contact(unsigned char* givenUUID, char const* givenName);
   Contact(unsigned char* givenUUID, char givenName);
-  ~Contact();
   void setUUID(unsigned char* givenUUID);
   void setName(char const* givenName);
   void setName(char givenName);
@@ -17,8 +13,8 @@ class Contact {
   char* getName();
 
  private:
-  unsigned char* uuid;
-  char* name;
+  unsigned char uuid[5];
+  char name[11];
 };
 
 #endif
